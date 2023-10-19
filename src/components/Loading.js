c37c6d0bee8af05f7b055850d3b7daf000fcc8ef
박은성 <cro3179@naver.com>
2023-10-20 01:30:27 +0900

    로딩창 변경

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -7,7 +7,7 @@ function Loading() {
     <div className="fixed inset-0 flex items-center justify-center z-10">
             <div className="max-w-[380px] h-[100vh] bg-white flex justify-center mx-auto items-center flex-col">
            
-                <img src="./../Images/loading.gif" className='mx-auto' alt="loading" />
+                <img src="./../Images/loading.gif" className='w-[50%]' alt="loading" />
             <p className='animate__animated animate__bounce mt-4 text-lg'>시훈이 첫 생일</p>
             <p className='animate__animated animate__bounce mt-4 text-lg'>초대합니다</p>
             </div>
