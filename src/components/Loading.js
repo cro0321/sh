45a59abfc8182168a158dc3df006353d1fe5d3d4
박은성 <cro3179@naver.com>
2023-10-20 01:09:19 +0900

    로딩창 애니메이트 추가

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -5,11 +5,18 @@ function Loading() {
 
     <>    
     <div className="fixed inset-0 flex items-center justify-center z-10">
-            <div className="max-w-[380px] h-[100vh] bg-white flex items-center">
+            <div className="max-w-[380px] h-[100vh] bg-white flex justify-center mx-auto items-center flex-col">
+           
                 <img src="./../Images/loading.gif" className='mx-auto' alt="loading" />
+            <p className='animate__animated animate__bounce mt-4 text-lg'>시훈이 첫 생일</p>
+            <p className='animate__animated animate__bounce mt-4 text-lg'>초대합니다</p>
             </div>
-
+          
         </div>
+        <div className="max-w-[380px] h-[100vh]">
+           
+       
+       </div>
         </>
 
   )
