743b493bac6fa0b4078b64df994979607f07302c
박은성 <cro3179@naver.com>
2023-10-21 03:53:44 +0900

    image 확장자 png 수정

--- src/components/Album.js
+++ src/components/Album.js
@@ -29,18 +29,16 @@ function Album() {
         modules={[Autoplay, Pagination]}
         className="mySwiper"
       >
-        <SwiperSlide><img src="./../Images/1.jpg" alt="시훈" /></SwiperSlide>
-        <SwiperSlide><img src="./../Images/2.jpg" alt="시훈" /></SwiperSlide>
-        <SwiperSlide><img src="./../Images/3.jpg" alt="시훈" /></SwiperSlide>
-        <SwiperSlide><img src="./../Images/4.jpg" alt="시훈" /></SwiperSlide>
-        <SwiperSlide><img src="./../Images/5.jpg" alt="시훈" /></SwiperSlide>
-        <SwiperSlide><img src="./../Images/6.jpg" alt="시훈" /></SwiperSlide>
-        <SwiperSlide><img src="./../Images/7.jpg" alt="시훈" /></SwiperSlide>
-        <SwiperSlide><img src="./../Images/8.jpg" alt="시훈" /></SwiperSlide>
-        <SwiperSlide><img src="./../Images/9.jpg" alt="시훈" /></SwiperSlide>
-        <SwiperSlide><img src="./../Images/10.jpg" alt="시훈" /></SwiperSlide>
-        <SwiperSlide><img src="./../Images/11.png" alt="시훈" /></SwiperSlide>
-        <SwiperSlide><img src="./../Images/12.webp" alt="시훈" /></SwiperSlide>
+        <SwiperSlide><img src="./../Images/1.png" alt="시훈" /></SwiperSlide>
+        <SwiperSlide><img src="./../Images/2.png" alt="시훈" /></SwiperSlide>
+        <SwiperSlide><img src="./../Images/3.png" alt="시훈" /></SwiperSlide>
+        <SwiperSlide><img src="./../Images/4.png" alt="시훈" /></SwiperSlide>
+        <SwiperSlide><img src="./../Images/5.png" alt="시훈" /></SwiperSlide>
+        <SwiperSlide><img src="./../Images/6.png" alt="시훈" /></SwiperSlide>
+        <SwiperSlide><img src="./../Images/7.png" alt="시훈" /></SwiperSlide>
+        <SwiperSlide><img src="./../Images/8.png" alt="시훈" /></SwiperSlide>
+        <SwiperSlide><img src="./../Images/9.png" alt="시훈" /></SwiperSlide>
+        <SwiperSlide><img src="./../Images/10.png" alt="시훈" /></SwiperSlide>
       </Swiper>
 
     </>
