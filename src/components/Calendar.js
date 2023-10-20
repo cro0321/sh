3b02675005b3fcaa306c83728d9471ed5a85f79e
박은성 <cro3179@naver.com>
2023-10-21 00:44:44 +0900

    재배포

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -24,8 +24,8 @@ function Calendar() {
 
       let dateInWeek = new Date(year, month, i).getDay();
       if (i === 24) {
-        datesArray.push(<div key={i} className="py-2  h-10 "><img className='
-         w-6 h-6 block mx-auto'  src="./../Images/loading.gif" alt="시훈" /></div>);
+        datesArray.push(<div key={i} className="-my-[11px] ml-1  h-10 "><img className='
+         w-14 h-14 block mx-auto'  src="./../Images/Calendar.gif" alt="시훈" /></div>);
         // <img  src="./../Images/1.jpg" alt="시훈" />
         //<FontAwesomeIcon icon={faCakeCandles} className='text-yellow-400 text-2xl' />
       } else if(dateInWeek === 0) {
